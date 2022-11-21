1890272d2acd460063ad6564ab6ff298126445ed
Daniel Lage <daniellc3221@gmail.com>
2022-11-21 01:04:51 -0300

    fixed bug that made it not add songs on android

--- App.js
+++ App.js
@@ -22,7 +22,11 @@ export default function App() {
     window.location.assign(
       "https://accounts.spotify.com/authorize?" + params.toString()
     );
-  } else if (!refresh_token) {
+    return;
+  }
+
+  if (!refresh_token) {
+    // get refresh token
     request.post(
       {
         url: "https://accounts.spotify.com/api/token",
@@ -38,45 +42,7 @@ export default function App() {
         json: true,
       },
       (error, response, body) => {
-        if (body.access_token) {
-          access_token = body.access_token;
-          refresh_token = body.refresh_token;
-
-          request.post(
-            {
-              url: "https://accounts.spotify.com/api/token",
-              form: {
-                refresh_token: refresh_token,
-                grant_type: "refresh_token",
-              },
-              headers: {
-                Authorization:
-                  "Basic ZWQxMjMyODcxMTMzNDVjNDkzMzhkMWNmMjBiZWM5MGU6NjE2Mzg1ZjJlYzNkNGQ2M2E3OWJkMWIxZGZhM2M4MGM=",
-              },
-              json: true,
-            },
-            (error, response, body) => {
-              const access_token = body.access_token;
-              request.get(
-                {
-                  url: "https://api.spotify.com/v1/me/playlists",
-                  headers: { Authorization: "Bearer " + access_token },
-                  json: true,
-                },
-                (error, response, body) => {
-                  setPlaylists(
-                    body.items.map((playlist) => ({
-                      id: playlist.id,
-                      name: playlist.name,
-                      tracks: playlist.tracks.href,
-                      image: playlist.images[0].url,
-                    }))
-                  );
-                }
-              );
-            }
-          );
-        } else {
+        if (!body.access_token) {
           const params = new URLSearchParams({
             response_type: "code",
             client_id: "ed123287113345c49338d1cf20bec90e",
@@ -86,7 +52,45 @@ export default function App() {
           window.location.assign(
             "https://accounts.spotify.com/authorize?" + params.toString()
           );
+          return;
         }
+        access_token = body.access_token;
+        refresh_token = body.refresh_token;
+        // get playlists
+        request.post(
+          {
+            url: "https://accounts.spotify.com/api/token",
+            form: {
+              refresh_token: refresh_token,
+              grant_type: "refresh_token",
+            },
+            headers: {
+              Authorization:
+                "Basic ZWQxMjMyODcxMTMzNDVjNDkzMzhkMWNmMjBiZWM5MGU6NjE2Mzg1ZjJlYzNkNGQ2M2E3OWJkMWIxZGZhM2M4MGM=",
+            },
+            json: true,
+          },
+          (error, response, body) => {
+            const access_token = body.access_token;
+            request.get(
+              {
+                url: "https://api.spotify.com/v1/me/playlists",
+                headers: { Authorization: "Bearer " + access_token },
+                json: true,
+              },
+              (error, response, body) => {
+                setPlaylists(
+                  body.items.map((playlist) => ({
+                    id: playlist.id,
+                    name: playlist.name,
+                    tracks: playlist.tracks.href,
+                    image: playlist.images[0].url,
+                  }))
+                );
+              }
+            );
+          }
+        );
       }
     );
   }
@@ -115,7 +119,9 @@ export default function App() {
           },
           (error, response, body) => {
             if (!body) {
-              alert("spotify não esta aberto");
+              alert(
+                "spotify não esta aberto (toque uma musica para ativar o dispositivo)"
+              );
               return;
             }
             const device_id = body.device.id;
@@ -128,8 +134,17 @@ export default function App() {
               },
               (error, response, body) => {
                 if (body.queue.length > 1) {
-                  alert("você ja tem musicas na fila");
-                  return;
+                  console.log(body);
+                  if (
+                    !body.queue.every(
+                      (track) => track.id == body.currently_playing.id // when you add single songs on spotify for android it adds 10 versions of it in the queue :) i totally dont want to kill
+                    )
+                  ) {
+                    alert(
+                      "você ja tem musicas na fila (se não tiver musicas na fila pesquise uma musica e toque ela)"
+                    );
+                    return;
+                  }
                 }
                 request.get(
                   {
@@ -217,23 +232,23 @@ export default function App() {
                                   });
                                 }
                               );
-                            } else {
-                              tracks.forEach((track, index) => {
-                                const query = new URLSearchParams({
-                                  uri: track.uri,
-                                  device_id: device_id,
-                                });
-                                request.post({
-                                  url:
-                                    "https://api.spotify.com/v1/me/player/queue?" +
-                                    query.toString(),
-                                  headers: {
-                                    Authorization: "Bearer " + access_token,
-                                  },
-                                  json: true,
-                                });
-                              });
+                              return;
                             }
+                            tracks.forEach((track, index) => {
+                              const query = new URLSearchParams({
+                                uri: track.uri,
+                                device_id: device_id,
+                              });
+                              request.post({
+                                url:
+                                  "https://api.spotify.com/v1/me/player/queue?" +
+                                  query.toString(),
+                                headers: {
+                                  Authorization: "Bearer " + access_token,
+                                },
+                                json: true,
+                              });
+                            });
                           }
                         );
                       }

571328d66c71cc694b3093a48fb8e9035cc4beaf
DANIEL LAGE CORTEZ <2019308264@IFAL.LOCAL>
2022-12-22 17:43:06 -0300

    added react navigation and playlist screen

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { useState } from "react";
-import { Button, Pressable, StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./src/home";
+import Playlist from "./src/playlist";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./src/load";
 import request from "request";
-import Icon from "./src/icon";
 
-var refresh_token;
+const Stack = createNativeStackNavigator();
+
 var auth_code;
 
 const colors = {
   primary: "hsl(216, 60%, 60%)",
   secondary: "hsl(216, 35%, 35%)",
   background: "hsl(216, 10%, 10%)",
+  default: "hsl(216, 75%, 70%)",
+  opening: "hsl(216, 20%, 20%)",
 };
 
-export default function App() {
-  const [playlists, setPlaylists] = useState([]);
+export default function () {
   const params = new URLSearchParams(window.location.search);
+  const [refreshToken, setRefreshToken] = useState();
 
   function refresh() {
     const params = new URLSearchParams({
@@ -32,249 +38,62 @@ export default function App() {
     );
   }
 
-  if (!params.get("code")) {
-    refresh();
-    return <View style={styles.container} />;
-  } else {
-    auth_code = params.get("code");
-  }
+  useEffect(() => {
+    if (!params.get("code")) {
+      refresh();
+      return <View style={styles.container} />;
+    } else {
+      auth_code = params.get("code");
+    }
 
-  if (!refresh_token) {
-    // get refresh token
-    request.post(
-      {
-        url: "https://accounts.spotify.com/api/token",
-        form: {
-          code: auth_code,
-          redirect_uri: window.location.origin,
-          grant_type: "authorization_code",
+    if (!refreshToken) {
+      // get refresh token
+      request.post(
+        {
+          url: "https://accounts.spotify.com/api/token",
+          form: {
+            code: auth_code,
+            redirect_uri: window.location.origin,
+            grant_type: "authorization_code",
+          },
+          headers: {
+            Authorization:
+              "Basic ZWQxMjMyODcxMTMzNDVjNDkzMzhkMWNmMjBiZWM5MGU6NjE2Mzg1ZjJlYzNkNGQ2M2E3OWJkMWIxZGZhM2M4MGM=",
+          },
+          json: true,
         },
-        headers: {
-          Authorization:
-            "Basic ZWQxMjMyODcxMTMzNDVjNDkzMzhkMWNmMjBiZWM5MGU6NjE2Mzg1ZjJlYzNkNGQ2M2E3OWJkMWIxZGZhM2M4MGM=",
-        },
-        json: true,
-      },
-      (error, response, body) => {
-        if (body.error) {
-          refresh();
-          return;
+        (error, response, body) => {
+          if (body.error) {
+            refresh();
+            return;
+          }
+          setRefreshToken(body.refresh_token);
         }
-        const access_token = body.access_token;
-        refresh_token = body.refresh_token;
-        // get playlists
-        request.post(
-          {
-            url: "https://accounts.spotify.com/api/token",
-            form: {
-              refresh_token: refresh_token,
-              grant_type: "refresh_token",
-            },
-            headers: {
-              Authorization:
-                "Basic ZWQxMjMyODcxMTMzNDVjNDkzMzhkMWNmMjBiZWM5MGU6NjE2Mzg1ZjJlYzNkNGQ2M2E3OWJkMWIxZGZhM2M4MGM=",
-            },
-            json: true,
-          },
-          (error, response, body) => {
-            const access_token = body.access_token;
-            request.get(
-              {
-                url: "https://api.spotify.com/v1/me/playlists",
-                headers: { Authorization: "Bearer " + access_token },
-                json: true,
-              },
-              (error, response, body) => {
-                setPlaylists(
-                  body.items.map((playlist) => ({
-                    id: playlist.id,
-                    name: playlist.name,
-                    tracks: playlist.tracks.href,
-                    image:
-                      playlist.images.length === 0
-                        ? null
-                        : playlist.images[0].url,
-                  }))
-                );
-              }
-            );
-          }
-        );
-      }
-    );
-    return;
-  }
+      );
+      return;
+    }
+  }, []);
 
-  function addToQueue(href) {
-    request.post(
-      {
-        url: "https://accounts.spotify.com/api/token",
-        form: {
-          refresh_token: refresh_token,
-          grant_type: "refresh_token",
-        },
-        headers: {
-          Authorization:
-            "Basic ZWQxMjMyODcxMTMzNDVjNDkzMzhkMWNmMjBiZWM5MGU6NjE2Mzg1ZjJlYzNkNGQ2M2E3OWJkMWIxZGZhM2M4MGM=",
-        },
-        json: true,
-      },
-      (error, response, body) => {
-        const access_token = body.access_token;
-
-        request.get(
-          {
-            url: "https://api.spotify.com/v1/me/player",
-            headers: { Authorization: "Bearer " + access_token },
-            json: true,
-          },
-          (error, response, body) => {
-            if (!body) {
-              alert(
-                "spotify não esta aberto (toque uma musica para ativar o dispositivo)"
-              );
-              return;
-            }
-            const device_id = body.device.id;
-
-            request.get(
-              {
-                url: href,
-                headers: { Authorization: "Bearer " + access_token },
-                json: true,
-              },
-              (error, response, body) => {
-                function get_shuffled_array(array) {
-                  const newArray = [];
-                  while (array.length) {
-                    const index = Math.floor(Math.random() * array.length);
-                    newArray.push(array.splice(index, 1)[0]);
-                  }
-                  return newArray;
-                }
-
-                const tracks = get_shuffled_array(
-                  body.items.map((value) => value.track)
-                );
-
-                const query = new URLSearchParams({
-                  uri: tracks.pop().uri,
-                  device_id: device_id,
-                });
-
-                request.post(
-                  {
-                    url:
-                      "https://api.spotify.com/v1/me/player/queue?" +
-                      query.toString(),
-                    headers: { Authorization: "Bearer " + access_token },
-                    json: true,
-                  },
-                  (error, response, body) => {
-                    if (body) {
-                      console.log("request: add first track to queue", body);
-                      return;
-                    }
-                    const query = new URLSearchParams({
-                      device_id: device_id,
-                    });
-                    request.post(
-                      {
-                        url:
-                          "https://api.spotify.com/v1/me/player/next?" +
-                          query.toString(),
-                        headers: {
-                          Authorization: "Bearer " + access_token,
-                        },
-                        json: true,
-                      },
-                      (error, response, body) => {
-                        if (body) {
-                          console.log("request: skip track", body);
-                          return;
-                        }
-                        tracks.forEach((track) => {
-                          const query = new URLSearchParams({
-                            uri: track.uri,
-                            device_id: device_id,
-                          });
-                          request.post({
-                            url:
-                              "https://api.spotify.com/v1/me/player/queue?" +
-                              query.toString(),
-                            headers: {
-                              Authorization: "Bearer " + access_token,
-                            },
-                            json: true,
-                          });
-                        });
-                      }
-                    );
-                  }
-                );
-              }
-            );
-          }
-        );
-      }
-    );
-  }
+  if (!refreshToken) return <LoadingScreen colors={colors} />;
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Adiciona Playlist Embaralhada Para Fila</Text>
-      <View style={styles.grid}>
-        {playlists.map((playlist) => (
-          <Icon
-            key={playlist.id}
-            playlist={playlist}
-            onPress={() => addToQueue(playlist.tracks)}
-          />
-        ))}
-      </View>
-      <Pressable
-        style={styles.button}
-        onPress={() => {
-          refresh();
-        }}
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: false }}
       >
-        <Text style={styles.text}>Recarregar</Text>
-      </Pressable>
-    </View>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: "Home Screen" }}
+          initialParams={{ refreshToken: refreshToken, colors: colors }}
+        />
+        <Stack.Screen
+          name="Playlist"
+          component={Playlist}
+          options={{ title: "Playlist" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-    height: "100%",
-    alignItems: "center",
-    backgroundColor: colors.background,
-    gap: 20,
-  },
-  grid: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    padding: 10,
-    backgroundColor: colors.secondary,
-    gap: 3,
-  },
-  title: {
-    fontSize: "300%",
-    fontWeight: "bold",
-    color: colors.primary,
-    textAlign: "center",
-    padding: 10,
-  },
-  button: {
-    padding: 10,
-    backgroundColor: colors.primary,
-    borderRadius: 5,
-  },
-  text: {
-    fontSize: "200%",
-    fontWeight: "bold",
-    color: colors.background,
-  },
-});

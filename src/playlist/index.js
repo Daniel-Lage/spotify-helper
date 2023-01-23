9e35c0948d317ebe7a4554656af448e8c458936c
= <=>
2023-01-22 23:48:07 -0300

    added support for playlists over 100 songs and for accounts with over 20 playlists

--- src/playlist/index.js
+++ src/playlist/index.js
@@ -50,217 +50,187 @@ export default function Playlist({
   }, []);
 
   function addToQueue() {
-    request.post(
-      {
-        url: "https://accounts.spotify.com/api/token",
-        form: {
-          refresh_token: refreshToken,
-          grant_type: "refresh_token",
+    useToken((accessToken) => {
+      request.get(
+        {
+          url: "https://api.spotify.com/v1/me/player",
+          headers: { Authorization: "Bearer " + accessToken },
+          json: true,
         },
-        headers: {
-          Authorization:
-            "Basic ZWQxMjMyODcxMTMzNDVjNDkzMzhkMWNmMjBiZWM5MGU6NjE2Mzg1ZjJlYzNkNGQ2M2E3OWJkMWIxZGZhM2M4MGM=",
-        },
-        json: true,
-      },
-      (error, response, body) => {
-        const access_token = body.access_token;
-        request.get(
-          {
-            url: "https://api.spotify.com/v1/me/player",
-            headers: { Authorization: "Bearer " + access_token },
-            json: true,
-          },
-          (error, response, body) => {
-            if (!body) {
-              alert(
-                "spotify não esta aberto (toque uma musica para ativar o dispositivo)"
-              );
-              return;
-            }
-            const device_id = body.device.id;
-
-            function get_shuffled_array(array) {
-              const newArray = [];
-              while (array.length) {
-                const index = Math.floor(Math.random() * array.length);
-                newArray.push(array.splice(index, 1)[0]);
-              }
-              return newArray;
-            }
-
-            const shuffledTracks = get_shuffled_array(
-              tracks.map((value) => value.track)
+        (error, response, body) => {
+          if (!body) {
+            alert(
+              "spotify não esta aberto (toque uma musica para ativar o dispositivo)"
             );
-
-            const query = new URLSearchParams({
-              uri: shuffledTracks.pop().uri,
-              device_id: device_id,
-            });
-
-            request.post(
-              {
-                url:
-                  "https://api.spotify.com/v1/me/player/queue?" +
-                  query.toString(),
-                headers: { Authorization: "Bearer " + access_token },
-                json: true,
-              },
-              (error, response, body) => {
-                if (body) {
-                  console.log("request: add first track to queue", body);
-                  return;
-                }
-                const query = new URLSearchParams({
-                  device_id: device_id,
-                });
-                request.post(
-                  {
-                    url:
-                      "https://api.spotify.com/v1/me/player/next?" +
-                      query.toString(),
-                    headers: {
-                      Authorization: "Bearer " + access_token,
-                    },
-                    json: true,
-                  },
-                  (error, response, body) => {
-                    if (body) {
-                      console.log("request: skip track", body);
-                      return;
-                    }
-                    shuffledTracks.forEach((track) => {
-                      const query = new URLSearchParams({
-                        uri: track.uri,
-                        device_id: device_id,
-                      });
-                      request.post({
-                        url:
-                          "https://api.spotify.com/v1/me/player/queue?" +
-                          query.toString(),
-                        headers: {
-                          Authorization: "Bearer " + access_token,
-                        },
-                        json: true,
-                      });
-                    });
-                  }
-                );
-              }
-            );
+            return;
           }
-        );
-      }
-    );
+          const device_id = body.device.id;
+
+          function get_shuffled_array(array) {
+            const newArray = [];
+            while (array.length) {
+              const index = Math.floor(Math.random() * array.length);
+              newArray.push(array.splice(index, 1)[0]);
+            }
+            return newArray;
+          }
+
+          const shuffledTracks = get_shuffled_array(
+            tracks.map((value) => value.track)
+          );
+
+          const query = new URLSearchParams({
+            uri: shuffledTracks.pop().uri,
+            device_id: device_id,
+          });
+
+          request.post(
+            {
+              url:
+                "https://api.spotify.com/v1/me/player/queue?" +
+                query.toString(),
+              headers: { Authorization: "Bearer " + accessToken },
+              json: true,
+            },
+            (error, response, body) => {
+              if (body) {
+                console.log("request: add first track to queue", body);
+                return;
+              }
+              const query = new URLSearchParams({
+                device_id: device_id,
+              });
+              request.post(
+                {
+                  url:
+                    "https://api.spotify.com/v1/me/player/next?" +
+                    query.toString(),
+                  headers: {
+                    Authorization: "Bearer " + accessToken,
+                  },
+                  json: true,
+                },
+                (error, response, body) => {
+                  if (body) {
+                    console.log("request: skip track", body);
+                    return;
+                  }
+                  shuffledTracks.forEach((track) => {
+                    const query = new URLSearchParams({
+                      uri: track.uri,
+                      device_id: device_id,
+                    });
+                    request.post({
+                      url:
+                        "https://api.spotify.com/v1/me/player/queue?" +
+                        query.toString(),
+                      headers: {
+                        Authorization: "Bearer " + accessToken,
+                      },
+                      json: true,
+                    });
+                  });
+                }
+              );
+            }
+          );
+        }
+      );
+    });
   }
 
   function addToQueueStartingFromSong(index) {
-    request.post(
-      {
-        url: "https://accounts.spotify.com/api/token",
-        form: {
-          refresh_token: refreshToken,
-          grant_type: "refresh_token",
+    useToken((accessToken) => {
+      request.get(
+        {
+          url: "https://api.spotify.com/v1/me/player",
+          headers: { Authorization: "Bearer " + accessToken },
+          json: true,
         },
-        headers: {
-          Authorization:
-            "Basic ZWQxMjMyODcxMTMzNDVjNDkzMzhkMWNmMjBiZWM5MGU6NjE2Mzg1ZjJlYzNkNGQ2M2E3OWJkMWIxZGZhM2M4MGM=",
-        },
-        json: true,
-      },
-      (error, response, body) => {
-        const access_token = body.access_token;
-        request.get(
-          {
-            url: "https://api.spotify.com/v1/me/player",
-            headers: { Authorization: "Bearer " + access_token },
-            json: true,
-          },
-          (error, response, body) => {
-            if (!body) {
-              alert(
-                "spotify não esta aberto (toque uma musica para ativar o dispositivo)"
-              );
-              return;
-            }
-            const device_id = body.device.id;
-
-            function get_shuffled_array(array) {
-              const newArray = [];
-              while (array.length) {
-                const index = Math.floor(Math.random() * array.length);
-                newArray.push(array.splice(index, 1)[0]);
-              }
-              return newArray;
-            }
-
-            const newTracks = [...tracks];
-            const first_track = newTracks.splice(index, 1)[0].track;
-
-            const shuffledTracks = get_shuffled_array(
-              newTracks.map((value) => value.track)
+        (error, response, body) => {
+          if (!body) {
+            alert(
+              "spotify não esta aberto (toque uma musica para ativar o dispositivo)"
             );
-            shuffledTracks.slice;
-
-            const query = new URLSearchParams({
-              uri: first_track.uri,
-              device_id: device_id,
-            });
-
-            request.post(
-              {
-                url:
-                  "https://api.spotify.com/v1/me/player/queue?" +
-                  query.toString(),
-                headers: { Authorization: "Bearer " + access_token },
-                json: true,
-              },
-              (error, response, body) => {
-                if (body) {
-                  console.log("request: add first track to queue", body);
-                  return;
-                }
-                const query = new URLSearchParams({
-                  device_id: device_id,
-                });
-                request.post(
-                  {
-                    url:
-                      "https://api.spotify.com/v1/me/player/next?" +
-                      query.toString(),
-                    headers: {
-                      Authorization: "Bearer " + access_token,
-                    },
-                    json: true,
-                  },
-                  (error, response, body) => {
-                    if (body) {
-                      console.log("request: skip track", body);
-                      return;
-                    }
-                    shuffledTracks.forEach((track) => {
-                      const query = new URLSearchParams({
-                        uri: track.uri,
-                        device_id: device_id,
-                      });
-                      request.post({
-                        url:
-                          "https://api.spotify.com/v1/me/player/queue?" +
-                          query.toString(),
-                        headers: {
-                          Authorization: "Bearer " + access_token,
-                        },
-                        json: true,
-                      });
-                    });
-                  }
-                );
-              }
-            );
+            return;
           }
-        );
-      }
-    );
+          const device_id = body.device.id;
+
+          function get_shuffled_array(array) {
+            const newArray = [];
+            while (array.length) {
+              const index = Math.floor(Math.random() * array.length);
+              newArray.push(array.splice(index, 1)[0]);
+            }
+            return newArray;
+          }
+
+          const newTracks = [...tracks];
+          const first_track = newTracks.splice(index, 1)[0].track;
+
+          const shuffledTracks = get_shuffled_array(
+            newTracks.map((value) => value.track)
+          );
+          shuffledTracks.slice;
+
+          const query = new URLSearchParams({
+            uri: first_track.uri,
+            device_id: device_id,
+          });
+
+          request.post(
+            {
+              url:
+                "https://api.spotify.com/v1/me/player/queue?" +
+                query.toString(),
+              headers: { Authorization: "Bearer " + accessToken },
+              json: true,
+            },
+            (error, response, body) => {
+              if (body) {
+                console.log("request: add first track to queue", body);
+                return;
+              }
+              const query = new URLSearchParams({
+                device_id: device_id,
+              });
+              request.post(
+                {
+                  url:
+                    "https://api.spotify.com/v1/me/player/next?" +
+                    query.toString(),
+                  headers: {
+                    Authorization: "Bearer " + accessToken,
+                  },
+                  json: true,
+                },
+                (error, response, body) => {
+                  if (body) {
+                    console.log("request: skip track", body);
+                    return;
+                  }
+                  shuffledTracks.forEach((track) => {
+                    const query = new URLSearchParams({
+                      uri: track.uri,
+                      device_id: device_id,
+                    });
+                    request.post({
+                      url:
+                        "https://api.spotify.com/v1/me/player/queue?" +
+                        query.toString(),
+                      headers: {
+                        Authorization: "Bearer " + accessToken,
+                      },
+                      json: true,
+                    });
+                  });
+                }
+              );
+            }
+          );
+        }
+      );
+    });
   }
 
   if (!tracks) return <LoadingScreen />;

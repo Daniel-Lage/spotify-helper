2d5fb801b411af03f8479e651ee71c58d085040a
= <=>
2023-02-09 19:02:50 -0300

    changed scrolling

---
+++ src/components/cover.js
@@ -0,0 +1,35 @@
+import { Image, View } from "react-native";
+import PlayButton from "./playbutton";
+
+export default function Cover({ colors, image, size }) {
+  return (
+    <View
+      style={{
+        alignItems: "flex-end",
+        justifyContent: "flex-end",
+        shadowOpacity: 0.3,
+        shadowRadius: 15,
+      }}
+    >
+      <Image
+        source={image}
+        style={{
+          width: `${size}vw`,
+          height: `${size}vw`,
+        }}
+      />
+      <PlayButton
+        colors={colors}
+        onPress={() => {
+          addToQueue();
+        }}
+        symbol="play"
+        style={{
+          margin: `${size / 6}vw`,
+          position: "absolute",
+        }}
+        size={size / 4}
+      />
+    </View>
+  );
+}
